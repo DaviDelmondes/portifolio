d30992657e1fafae38a64905db82aba24aba1b76
DaviDelmondes <Davidnascimentomederos40@gmail.com>
2024-01-25 12:49:03 -0300

    comit

--- js/script.js
+++ js/script.js
@@ -23,6 +23,13 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
              });
+             //active section for animation on scroll
+             sec.classList.add('show-animate');
+             
+        }
+        //if waant to use animation that repeats on scroll use thi
+        else{
+            sec.classList.remove('show-animate');
         }
     });
 
@@ -30,4 +37,8 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+   // remove toggle icon and navbar when clink navbar links (scroll)
+   menuIcon.classList.remove('bx-x');
+   navbar.classList.remove('active');
 }
